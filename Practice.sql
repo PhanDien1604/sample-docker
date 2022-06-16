3782e958c1bf75f5437b626ed90cb45d026f8a51
Phandien1604 <dienpq1604@gmail.com>
2022-06-16 16:30:29 +0700

    PHP sql

---
+++ Practice.sql
@@ -0,0 +1,24 @@
+--Cau 1
+SELECT categories.*, count(items.id) as count_items 
+FROM categories 
+LEFT JOIN items on categories.id = items.category_id
+GROUP BY categories.id;
+
+--Cau 2
+SELECT categories.*, sum(items.amount) as sum_items_amount 
+FROM categories 
+LEFT JOIN items on categories.id = items.category_id
+GROUP BY categories.id;
+
+--Cau 3
+SELECT categories.*, items.amount FROM categories 
+INNER JOIN (SELECT * FROM items WHERE amount > 40) AS items
+ON categories.id = items.category_id
+GROUP BY categories.id;
+
+--Cau 4
+DELETE FROM categories WHERE id NOT IN 
+(
+    SELECT DISTINCT category_id FROM items
+    WHERE category_id IS NOT NULL
+);
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
 echo 'Here the index file';
+
+try {
+    $dbh = new PDO('mysql:host=db;port=3306;dbname=sampleDB', 'root', 'root');
+    $dbh->exec('CREATe TABLE IF NOT EXISTS sample_table (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        sample_column VARCHAR(30) NOT NULL
+    );');
+    echo '<br>';
+    echo 'Conection success';
+} catch (PDOException $e) {
+    echo 'Error!:';
+    echo '</br>';
+    echo $e;
+    die();
+}
+
+
